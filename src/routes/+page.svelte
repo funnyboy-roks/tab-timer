2b3116b56fad2542ad5632a790f3ab26db66bcc4
iAmNsengi <iamnsengi@icloud.com>
2025-04-08 18:51:08 +0200

    feat: automatically infer hour when left blank

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -151,9 +151,10 @@
         console.log(alarm);
 
         let date = new Date();
-
+        
         if (!alarm.m) alarm.m = 0;
         if (!alarm.s) alarm.s = 0;
+        if (!alarm.h) alarm.h = date.getMinutes() > alarm.m ? date.getHours() + 1 : date.getHours()
 
         date.setHours(alarm.h || 0);
         date.setMinutes(alarm.m);
